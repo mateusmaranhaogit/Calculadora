a13ca081a20390e155b62ff29767c6bbbc1aed19
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-09-08 14:54:55 -0300

    Classe soma criada

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -1,3 +1,5 @@
+package Calculadora;
+
 public class Calculadora {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/Calculadora/Soma.java
@@ -0,0 +1,8 @@
+package Calculadora;
+
+//Criando a classe soma
+public class Soma {
+    public int somar(int a, int b){
+        return a + b; //Função de somar
+    }
+}
