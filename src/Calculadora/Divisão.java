364874432ca1910ef077778df9957db0358fd951
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-09-08 14:58:51 -0300

    Teste

---
+++ src/Calculadora/Divisão.java
@@ -0,0 +1,7 @@
+package Calculadora;
+
+public class Divisão {
+    public int dividir(int a, int b){
+        return a / b;//Função de dividir
+    }
+}
---
+++ src/Calculadora/Multiplicação.java
@@ -0,0 +1,7 @@
+package Calculadora;
+
+public class Multiplicação {
+    public int multiplicar(int a, int b){
+        return a * b;//Função de multiplicar
+    }
+}
