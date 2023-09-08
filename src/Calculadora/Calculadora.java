9da75a8197f70bb3bbc318c0eb22ce980c300987
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-09-08 14:59:49 -0300

    Classe calculadora criada

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -1,7 +1,19 @@
 package Calculadora;
 
 public class Calculadora {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public int somar(int a, int b){
+        return a + b;//Função de somar
+    }
+    
+    public int subtrair(int a, int b){
+        return a - b;//Função de subtrair
+    }
+    
+    public int multiplicar(int a, int b){
+        return a * b;//Função de multiplicar
+    }
+    
+    public int dividir(int a, int b){
+        return a / b;//Função de dividir
     }
 }
