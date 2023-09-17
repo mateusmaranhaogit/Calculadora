f4d2fee6f1389392e4add3f31e609aa11ec800c8
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-09-17 10:38:26 -0300

    Testes JUnit criados

--- src/Calculadora/CalculadoaJUnitTest.java
+++ src/Calculadora/CalculadoaJUnitTest.java
@@ -10,7 +10,6 @@ public class CalculadoaJUnitTest {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar(10,20);
         System.out.println(soma);
-
         Assert.assertEquals(30, soma); //Resultado esperado
         Assert.assertEquals(20, soma); //Resultado NÃO esperado
     }
@@ -20,28 +19,25 @@ public class CalculadoaJUnitTest {
         Calculadora calculadora = new Calculadora();
         int subtracao = calculadora.subtrair(20,10);
         System.out.println(subtracao);
-
         Assert.assertEquals(10, subtracao); //Resultado esperado
         Assert.assertEquals(20, subtracao); //Resultado NÃO esperado
     }   
     @Test
-    //Teste subtração
+    //Teste divisão
     public void divisaoDoisNumeros(){
         Calculadora calculadora = new Calculadora();
         int divisao = calculadora.dividir(20,10);
         System.out.println(divisao);
-
         Assert.assertEquals(2, divisao); //Resultado esperado
         Assert.assertEquals(1, divisao); //Resultado NÃO esperado
 
     }     
     @Test
-    //Teste subtração
+    //Teste multiplicação
     public void multiplicacaoDoisNumeros(){
         Calculadora calculadora = new Calculadora();
         int multiplicacao = calculadora.multiplicar(20,10);
         System.out.println(multiplicacao);
-
         Assert.assertEquals(200, multiplicacao); //Resultado esperado
         Assert.assertEquals(100, multiplicacao); //Resultado NÃO esperado
 
