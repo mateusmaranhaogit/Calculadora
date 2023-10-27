8a758addd6aab78ff2708073d6c378eddafcdbdc
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-10-26 22:01:19 -0300

    Código documentado

--- src/Calculadora/Divisão.java
+++ src/Calculadora/Divisão.java
@@ -1,7 +1,24 @@
+/**
+ * Classe que representa uma operação de divisão.
+ *
+ * Esta classe fornece um método para realizar a operação de divisão entre dois números inteiros.
+ *
+ * @author Mateus Maranhão
+ * @version 1.0         
+ * @since 21 de Setembro de 2023 
+ */
 package Calculadora;
 
 public class Divisão {
+    
+    /**
+     * Realiza a operação de divisão entre dois números inteiros.
+     *
+     * @param a O número que será dividido.
+     * @param b O número pelo qual será dividido (não pode ser zero).
+     * @return O resultado da divisão.
+     */
     public int dividir(int a, int b){
-        return a / b;//Função de dividir
+        return a / b;  // Função de dividir
     }
 }
--- src/Calculadora/TesteCalculadora.java
+++ src/Calculadora/TesteCalculadora.java
@@ -1,81 +1,89 @@
+/**
+ * Classe que contém cenários de teste para a classe Calculadora.
+ *
+ * Esta classe define vários cenários de teste para as operações de soma, subtração, multiplicação e divisão
+ * fornecidas pela classe Calculadora.
+ *
+ * @author Mateus Maranhão
+ * @version 1.0         
+ * @since 21 de Setembro de 2023 
+ */
 package Calculadora;
 
 public class TesteCalculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         
-//Cenário de testes de soma:
+        // Cenário de testes de soma:
 
-    //Teste 1: Soma de dois valores positivos
-            int soma = calc.somar(5,5);
-            System.out.println(soma);
-            
-     //Teste 2: Soma de dois valores positivos sendo um zero         
-            soma = calc.somar(5,0);
-            System.out.println(soma);
+        // Teste 1: Soma de dois valores positivos
+        int soma = calc.somar(5,5);
+        System.out.println(soma);
+        
+        // Teste 2: Soma de dois valores positivos sendo um zero         
+        soma = calc.somar(5,0);
+        System.out.println(soma);
 
-     //Teste 3: Soma de dois valores positivos ambos sendo zero 
-            soma = calc.somar(0,0);
-            System.out.println(soma);
+        // Teste 3: Soma de dois valores positivos ambos sendo zero 
+        soma = calc.somar(0,0);
+        System.out.println(soma);
 
-     //Teste 4: Soma de dois valores positivos sendo um negativo
-            soma = calc.somar(5,-1);
-            System.out.println(soma);
- 
-            
-//Cenário de testes de subtração:
+        // Teste 4: Soma de dois valores positivos sendo um negativo
+        soma = calc.somar(5,-1);
+        System.out.println(soma);
 
-    //Teste 1: Subtração de dois valores positivos
-            int subtracao = calc.subtrair(5,5);
-            System.out.println(subtracao);
-            
-     //Teste 2: Subtração de dois valores positivos sendo um zero         
-            subtracao = calc.subtrair(5,0);
-            System.out.println(subtracao);
+        // Cenário de testes de subtração:
 
-     //Teste 3: Subtração de dois valores positivos ambos sendo zero 
-            subtracao = calc.subtrair(0,0);
-            System.out.println(subtracao);
+        // Teste 1: Subtração de dois valores positivos
+        int subtracao = calc.subtrair(5,5);
+        System.out.println(subtracao);
+        
+        // Teste 2: Subtração de dois valores positivos sendo um zero         
+        subtracao = calc.subtrair(5,0);
+        System.out.println(subtracao);
 
-     //Teste 4: Subtração de dois valores positivos sendo um negativo
-            subtracao = calc.subtrair(5,-1);
-            System.out.println(subtracao);
-            
- //Cenário de testes de multiplicação:
+        // Teste 3: Subtração de dois valores positivos ambos sendo zero 
+        subtracao = calc.subtrair(0,0);
+        System.out.println(subtracao);
 
-    //Teste 1: Multiplicação de dois valores positivos
-            int multiplicacao = calc.multiplicar(5,5);
-            System.out.println(multiplicacao);
-            
-     //Teste 2: Multiplicação de dois valores positivos sendo um zero         
-            multiplicacao = calc.multiplicar(5,0);
-            System.out.println(multiplicacao);
+        // Teste 4: Subtração de dois valores positivos sendo um negativo
+        subtracao = calc.subtrair(5,-1);
+        System.out.println(subtracao);
+        
+        // Cenário de testes de multiplicação:
 
-     //Teste 3: Multiplicação de dois valores positivos ambos sendo zero 
-            multiplicacao = calc.multiplicar(0,0);
-            System.out.println(multiplicacao);
+        // Teste 1: Multiplicação de dois valores positivos
+        int multiplicacao = calc.multiplicar(5,5);
+        System.out.println(multiplicacao);
+        
+        // Teste 2: Multiplicação de dois valores positivos sendo um zero         
+        multiplicacao = calc.multiplicar(5,0);
+        System.out.println(multiplicacao);
 
-     //Teste 4: Multiplicação de dois valores positivos sendo um negativo
-            multiplicacao = calc.multiplicar(5,-1);
-            System.out.println(multiplicacao);
-            
-//Cenário de testes de divisão:
+        // Teste 3: Multiplicação de dois valores positivos ambos sendo zero 
+        multiplicacao = calc.multiplicar(0,0);
+        System.out.println(multiplicacao);
 
-    //Teste 1: Divisão de dois valores positivos
-            int divisao = calc.dividir(5,5);
-            System.out.println(divisao);
-            
-     //Teste 2: Divisão de dois valores positivos sendo um zero         
-            divisao = calc.dividir(5,0);
-            System.out.println(divisao);
+        // Teste 4: Multiplicação de dois valores positivos sendo um negativo
+        multiplicacao = calc.multiplicar(5,-1);
+        System.out.println(multiplicacao);
+        
+        // Cenário de testes de divisão:
 
-     //Teste 3: Divisão de dois valores positivos ambos sendo zero 
-            divisao = calc.dividir(0,0);
-            System.out.println(divisao);
+        // Teste 1: Divisão de dois valores positivos
+        int divisao = calc.dividir(5,5);
+        System.out.println(divisao);
+        
+        // Teste 2: Divisão de dois valores positivos sendo um zero         
+        divisao = calc.dividir(5,0);
+        System.out.println(divisao);
 
-     //Teste 4: Divisão de dois valores positivos sendo um negativo
-            divisao = calc.dividir(5,-1);
-            System.out.println(divisao);
+        // Teste 3: Divisão de dois valores positivos ambos sendo zero 
+        divisao = calc.dividir(0,0);
+        System.out.println(divisao);
+
+        // Teste 4: Divisão de dois valores positivos sendo um negativo
+        divisao = calc.dividir(5,-1);
+        System.out.println(divisao);
     }
-      
 }
